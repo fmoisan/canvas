8e6f98724d4cade12df151e969c3d39f20231e2b
François Moisan <francois.moisan@gmail.com>
2016-06-10 22:53:06 -0400

    msvc: enable /MP for parallel compilation

--- source/cmake/compiler/msvc.cmake
+++ source/cmake/compiler/msvc.cmake
@@ -9,6 +9,9 @@ set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /W4 /WX")
 # exceptions
 set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /EHsc")
 
+# parallel compilation
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /MP")
+
 if (MSVC11)
 	# fix std::tuple error from googletest usage
 	set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /D_VARIADIC_MAX=10")
